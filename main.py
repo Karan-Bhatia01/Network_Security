dbd34ed331a7835555c647f492732075f359c0b8
Karan-Bhatia01 <bhatiakaran168@gmail.com>
2025-03-15 14:35:50 +0530

    Data Validation Code Completed

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_validation import DataValidation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
@@ -12,7 +13,12 @@ if __name__=="__main__":
         data_ingestion=DataIngestion(dataingestionconfig)
         logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initation Completed")
         print(dataingestionartifact)
-        
+        data_validaiton_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validaiton_config)
+        logging.info("Intiate the data validation")
+        data_validaiton_artifact=data_validation.intitate_data_validation()
+        logging.info("data validation completed")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -14,6 +14,8 @@ FILE_NAME: str = "phisingData.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 
+SCHEMA_FILE_PATH = os.path.join("data_schema","schema.yaml")
+
 '''
 Data Ingestion related constant start with Data_Ingestion var name
 '''
@@ -23,3 +25,14 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_NAME: str = "feature_store"
 DATA_INGESTION_INGESTED_NAME: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
+
+"""
+Data Validation related constant start with DATA_VALIDATION VAR NAME
+"""
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift-report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+DATA_VALIDATION_INVALID_TRAIN_FILE_NAME: str = "invalid_train.csv"
+DATA_VALIDATION_INVALID_TEST_FILE_NAME: str = "invalid_test.csv"
